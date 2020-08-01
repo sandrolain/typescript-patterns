229d0a70d17e3e5feb0983334313491dd29e36bd
Sandro Lain <sandrolain@MBP-di-Sandro.station>
2020-08-01 15:15:12 +0200

    Typo correction

--- src/versioned-service-factory/ServiceFactory.spec.ts
+++ src/versioned-service-factory/ServiceFactory.spec.ts
@@ -4,22 +4,22 @@ import { NewFooService } from "./NewFooService";
 
 describe("ServiceFactory", () => {
 
-  test("Should create OldFooService passing versione 0.9.0", async () => {
+  test("Should create OldFooService passing version 0.9.0", async () => {
     const oldService = ServiceFactory.getFooService("https://example.com", "0.9.0");
     expect(oldService).toBeInstanceOf(OldFooService);
   });
 
-  test("Should create OldFooService passing versione 1.5.0", async () => {
+  test("Should create OldFooService passing version 1.5.0", async () => {
     const oldService = ServiceFactory.getFooService("https://example.com", "1.5.0");
     expect(oldService).toBeInstanceOf(OldFooService);
   });
 
-  test("Should create NewFooService passing versione 2.0.0", async () => {
+  test("Should create NewFooService passing version 2.0.0", async () => {
     const oldService = ServiceFactory.getFooService("https://example.com", "2.0.0");
     expect(oldService).toBeInstanceOf(NewFooService);
   });
 
-  test("Should create NewFooService passing versione 3.5.0", async () => {
+  test("Should create NewFooService passing version 3.5.0", async () => {
     const oldService = ServiceFactory.getFooService("https://example.com", "3.5.0");
     expect(oldService).toBeInstanceOf(NewFooService);
   });
